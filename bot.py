71d75218527711e76485884fa422fff2b4fd68b1
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-04-06 18:27:20 +0700

    docs: Add instalation instructions

--- bot.py
+++ bot.py
@@ -5,14 +5,15 @@ bot = Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
+    """
+        Jangan dihapus, bwt debug koneksi
+    """
+
     print('Bot is connected to these servers:')
 
     for guild in bot.guilds:
         print('{} ({})'.format(guild.name, guild.id))
 
 @bot.command(name='pif2')
-async def say(ctx, param1):
-    try:
-        await ctx.send(param1.lower())
-    except errors.MissingRequiredArgument:
-        return
+async def command_hub(ctx, *params):
+    await ctx.send('Steren Ganteng Sekali (SGS)') # placeholder
