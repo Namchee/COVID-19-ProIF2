eec8d32fd74bf08556ceafa136d29c4a757e4717
davidatz285 <55009370+davidatz285@users.noreply.github.com>
2020-04-08 22:42:42 +0700

    rapiin output bot

--- bot.py
+++ bot.py
@@ -18,13 +18,38 @@ Command List:
 """
 
     return await ctx.send(help_string)
+class Status(object):
+    def __init__(self, data):
+        if type(data) is str:
+            data = jsson.loads(data)
+        self.convert_json(data)
 
+    def convert_json(self, data):
+        self.__dict__ = {}
+        for key, value in data.items():
+            if type(value) is dict:
+                value = Status(value)
+            self.__dict__[key] = value
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+		
 async def handle_status(ctx, params):
     url = "https://covid19.mathdro.id/api/countries/"
     url += params[0]
-    response = requests.get(url)	
-    status = response.json()
-    return await ctx.send(status)
+    response = requests.get(url)
+    string = response.json()
+    status = Status(string)
+    text = "Perkembangan COVID-19 di " + params[0].capitalize() + ":\n\n"
+    text += "Jumlah Kasus Positif: " + str(status.confirmed.value) + "\n"
+    text += "Jumlah Pasien Sembuh: " + str(status.recovered.value) + "\n"
+    text += "Jumlah Pasien Meninggal: " + str(status.deaths.value) + "\n\n"	
+    text += "Terakhir Diperbarui : " + str(status.lastUpdate) + "\n"
+    text += "Data diambil dari JHE University"
+    return await ctx.send(text)
 
 handler_map = {}
 
