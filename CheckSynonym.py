043622754e40d49e0e6ab0e2c296a7226a06e71a
stephenhadi <stephenhadi123@gmail.com>
2020-04-09 13:14:39 +0700

    feat:check synonym

--- CheckSynonym.py
+++ CheckSynonym.py
@@ -1,22 +1,25 @@
 import json
 import requests
+from os import environ
+from dotenv import load_dotenv, find_dotenv
 
 
 class CheckSynonym:
     def __init__(self):
+        load_dotenv(find_dotenv())
         self.URL = "https://words.bighugelabs.com/api/2/"
-        self.API_KEY = 'b21ab0483ed4e31ed5c41cd8f5aac2d7/'
-        self.allCountries = self.fetch_all_countries()
+        self.API_KEY = environ.get("BIG_HUGE_THESAURUS_KEY")+"/"
+        self.allCountries = self.__fetch_all_countries()
         self.type = '/json'
 
-    def check_synonyms(self,textParam):
+    def check_synonyms(self, textParam):
         completeURL = self.URL+self.API_KEY+textParam+self.type
         sendRequest = requests.get(url = completeURL)
         data = sendRequest.json()
         synonymList = data["noun"]["syn"]
         result = "not found"
         for countries in synonymList:
-            result = self.binary_search(0, len(self.allCountries)-1, countries)
+            result = self.__binary_search(0, len(self.allCountries)-1, countries)
             if(result != 'empty'):
                 break
         return result
@@ -27,14 +30,13 @@ class CheckSynonym:
             if self.allCountries[mid]["name"].casefold() == searchedText.casefold():
                 return self.allCountries[mid]["name"]
             elif self.allCountries[mid]["name"] > searchedText:
-                return self.binary_search(left, mid-1, searchedText)
+                return self.__binary_search(left, mid-1, searchedText)
             else:
-                return self.binary_search(mid+1, right, searchedText)
+                return self.__binary_search(mid+1, right, searchedText)
         else:
             return "empty"
 
-    @staticmethod
-    def __fetch_all_countries():
+    def __fetch_all_countries(self):
         file = open("countries.txt", "r")
         content = file.read()
         jsonContent = json.loads(content)
