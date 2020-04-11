be09406e0c6cb3b3b2cc95aa30d0a137f069b087
Fariz Armesta <piisifaiz@gmail.com>
2020-04-11 12:42:39 +0700

    Commit info bot

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 from discord.ext.commands import Bot, errors
 from json import dumps
+import json
+import requests
+from datetime import datetime
+import time
 from CheckSynonym import CheckSynonym
 
 async def handle_help(ctx, params):
@@ -10,14 +14,83 @@ Command List:
 1. `help`
     - Usage: `!pif2 help`
     - Function: Show the help dialog
+
+2. `status <country name>`
+	- Usage: `!pif2 status <country name>`
+    - Function: Show status from given country 	
+
+3. `info`
+	- Usage: `!pif2 info`
+    - Function: Show summary info about COVID-19	 
 """
 
     return await ctx.send(help_string)
 
+class Status(object):
+    def __init__(self, data):
+        if type(data) is str:
+            data = jsson.loads(data)
+        self.convert_json(data)
+
+    def convert_json(self, data):
+        self.__dict__ = {}
+        for key, value in data.items():
+            if type(value) is dict:
+                value = Status(value)
+            self.__dict__[key] = value
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+		
+async def handle_status(ctx, params):
+    url = "https://covid19.mathdro.id/api/countries/"
+    fullParam = " "
+    fullParam = fullParam.join(params)
+    checked = synonymCheck.check_synonyms(fullParam)
+    if checked != None:
+        url += checked
+        response = requests.get(url)
+        string = response.json()
+        status = Status(string)
+        text = "COVID-19 Status in " + fullParam.capitalize() + ":\n\n"
+        text += "Infected: " + str(status.confirmed.value) + "\n"
+        text += "Recovered: " + str(status.recovered.value) + "\n"
+        text += "Dead: " + str(status.deaths.value) + "\n\n"	
+        text += "Last Updated : " + convert_datetime(str(status.lastUpdate)) + "\n"
+        text += "Data taken from JHE University"
+    else:
+	    text = "I'm sorry, looks like those country does not exist in our database"
+    return await ctx.send(text)
+
+async def handle_info(ctx, params):
+    url = "https://en.wikipedia.org/api/rest_v1/page/summary/Coronavirus_disease_2019"
+    response = requests.get(url)
+    string = response.json()
+    text = string['extract'] +'\n\n' + 'Information is taken from: Wikipedia'
+    return await ctx.send(text)
+
+def convert_datetime(strDate):
+    datetimeObj = strDate
+    datetimeObj = datetimeObj.replace('T',' ',1)
+    datetimeObj = datetimeObj[0:19]
+    datetimeObj = datetime.strptime(datetimeObj,"%Y-%m-%d %H:%M:%S")
+    hari = datetimeObj.strftime("%A")
+    tanggal = datetimeObj.strftime("%d")
+    bulan = datetimeObj.strftime("%B")
+    tahun = "20" + datetimeObj.strftime("%y")
+    jam = datetimeObj.strftime("%H")
+    menit = datetimeObj.strftime("%M")
+    result = hari + ", " + tanggal + " " + bulan + " " + tahun + " pukul " + jam + ":" + menit + " GMT+0"
+    return result
+	 
 handler_map = {}
 
 handler_map['help'] = handle_help # tulis handler functionnya diatas
-
+handler_map['status'] = handle_status
+handler_map['info'] = handle_info
 bot = Bot(command_prefix='!')
 
 @bot.event
@@ -25,6 +98,7 @@ async def on_ready():
     """
         Jangan dihapus, bwt debug koneksi
     """
+    global synonymCheck
     synonymCheck = CheckSynonym()
     # synonymCheck.check_synonyms(nama_negara) buat cek synonymnya kalau gak ketemu kembaliannya 'empty'
     print('Bot is connected to these servers:')
